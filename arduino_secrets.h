7bbcc6b3bbf3eb3aa271a5f24d459f51cff6c50d
Richard <richvern31@gmail.com>
2026-09-04 16:42:17 +0200

    Add files via upload

---
+++ arduino_secrets.h
@@ -0,0 +1,10 @@
+#include "Arduino.h"
+#define SECRET_SSID "Livebox-0A70"
+#define SECRET_PASS "hq5mqv7bjTt5YaC2MC"
+
+/*Dans l'entête du programme
+      Carte	                                  Syntaxe conseillée
+Arduino UNO R4 WiFi	char ssid[] = SECRET_SSID; et char pass[] = SECRET_PASS;
+         ou
+ARDUINO UNO R4 Wifi     const char* ssid = SECRET_SSID; et const char* pass = SECRET_PASS;
+*/
